7d15a53a8daaf3ad888343aa45fa62b390b1e349
fh137 <fh137@users.noreply.github.com>
2017-02-12 13:16:11 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+### Purpose
+How do you start a project with some of these technologies combined?  Here's a working starter project; something
+to examine & use as a short example.
+
+### Includes Use Of
+* gradle 3
+* Scala 2.11
+* logback / slf4j -- currently setup to write log output to stdout 
+* Vaadin 7
+* Gretty -- allows you to start web-app via gradle target 'appRun'
+
+### Targets
+* gradle clean appRun
+* http://localhost:8080/ScalaVaadin
+
---
+++ src/main/scala/com/fh/App.scala
@@ -0,0 +1,41 @@
+package com.fh
+
+import javax.servlet.annotation.WebServlet
+
+import com.vaadin.annotations.{Theme, Title, VaadinServletConfiguration}
+import com.vaadin.server.{VaadinRequest, VaadinServlet}
+import com.vaadin.ui._
+import org.slf4j.LoggerFactory
+
+/**
+  * Servlet definition with annotations, so that we can avoid using a web.xml file.
+  * Points to our sub-class of the Vaadin UI.
+  * Created by fh on 2/12/2017.
+  */
+@WebServlet(value=Array("/*"), asyncSupported=true)
+@VaadinServletConfiguration(productionMode=false, ui=classOf[App])
+class Servlet extends VaadinServlet
+
+/**
+  * Vaadin application.
+  * Created by fh on 2/12/2017.
+  */
+@Theme( "fh1" )
+@Title( "Hello World Application" )
+class App extends UI {
+  private val log = LoggerFactory.getLogger( classOf[App] )
+
+  override def init(request: VaadinRequest): Unit = {
+    try {
+      log.info( "init" )
+
+      val view = new VerticalLayout()
+      view.setStyleName( "bgYellow" )   // highlight the background of this view, so that we can see it on the page
+      view.addComponent(new Label("Hello Vaadin!"))
+
+      setContent(view)
+    } catch {
+      case t: Throwable => log.error( "Failed miserably", t )
+    }
+  }
+}
